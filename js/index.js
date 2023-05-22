5a71bda50779ce04578640c7a83b6aa590d760e4
Shahadat Hossain <shahadat3669@gmail.com>
2023-05-22 21:34:57 +0600

    Add past projects data.

---
+++ js/index.js
@@ -0,0 +1,27 @@
+// !! IMPORTANT README:
+
+// You may add additional external JS anurcts to work. BABEL must also be left in place.
+
+/** *********
+INSTRUCTIONS:
+  - Select the project you would
+    like to complete from the dropdown
+    menu.
+  - Click the "RUN TESTS" button to
+    run the tests against the blank
+    pen.
+  - Click the "TESTS" button to see
+    the individual test cases.
+    (should all be failing at first)
+  - Start coding! As you fulfill each
+    test case, you will see them go
+    from red to green.
+  - As you start to build out your
+    project, when tests are failing,
+    you should get helpful errors
+    along the way!
+    *********** */
+
+// PLEASE NOTE: Adding global style ite's CSS. Try adding: * { color: red }, for a quick example!
+
+// Once you have read the above messages, you can delete all comments.
